418f70e9ce8d47741562bfc301299dac0a5ff065
unclezap <zeb.blackwell@gmail.com>
2020-03-22 13:04:57 -0700

    bunny thing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 let dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "100px";
+// dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "100px";
 
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px","");
